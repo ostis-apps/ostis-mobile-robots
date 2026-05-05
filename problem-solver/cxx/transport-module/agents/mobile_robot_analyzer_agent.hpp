b73c049451a15cf064cacc85ce95cc5ef7835b3c
Kuzyonysh <dasha.kuzmich06@gmail.com>
2026-05-05 19:53:25 +0300

    Update mobile_robot_analyzer_agent.hpp

--- problem-solver/cxx/transport-module/agents/mobile_robot_analyzer_agent.hpp
+++ problem-solver/cxx/transport-module/agents/mobile_robot_analyzer_agent.hpp
@@ -1,1 +1,27 @@
+#pragma once
 
+#include <sc-memory/sc_agent.hpp>
+#include <chrono>
+#include <map>
+#include "keynodes/keynodes.hpp"
+
+using ScEventChangeMobileRobotState = ScEventAfterGenerateIncomingArc<ScType::ConstPosArc>;
+
+class MobileRobotAnalyzerAgent : public ScAgent<ScEventChangeMobileRobotState>
+{
+public:
+  ScAddr GetActionClass() const override;
+  bool CheckInitiationCondition(ScEventChangeMobileRobotState const & event) override;
+  ScResult DoProgram(ScEventChangeMobileRobotState const & event, ScAction & action) override;
+
+private:
+  void UpdateStatistic(ScAddr const & robotAddr, ScAddr const & statRelation, double deltaTime);
+  double ReadLinkValue(ScAddr const & linkAddr);
+  void WriteLinkValue(ScAddr const & linkAddr, double value);
+
+  std::map<size_t, std::chrono::steady_clock::time_point> m_startTimes;
+  std::map<size_t, ScAddr> m_lastStates;
+  
+  // Добавляем контекст памяти
+  ScMemoryContext* m_memoryCtx;
+};
