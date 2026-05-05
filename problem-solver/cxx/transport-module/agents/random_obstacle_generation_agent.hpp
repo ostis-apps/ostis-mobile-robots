a5f9861bcc9c8b562ee17fe4058712296af8ef16
ttanichh <solovejtatana66@gmail.com>
2026-05-05 13:57:49 +0300

    fix: hpp generator change

--- problem-solver/cxx/transport-module/agents/random_obstacle_generation_agent.hpp
+++ problem-solver/cxx/transport-module/agents/random_obstacle_generation_agent.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <random>
+#include <unordered_map>
 
 #include <sc-memory/sc_agent.hpp>
 
@@ -22,21 +23,29 @@ public:
 private:
   bool ShouldGenerateObstacle();
 
-  ScAddr GenerateObstacleAppearanceEvent(ScAddr const & tickAddr);
+  void RemoveExpiredObstacles();
 
-  ScAddr GenerateObstacle(ScAddr const & tickAddr);
+  void RemoveObstacle(ScAddr const & obstacleAddr);
+
+  ScAddr GenerateObstacle();
 
   ScAddr SelectObstaclePosition();
 
   int GenerateNextObstacleInterval();
 
+  int GenerateObstacleLifetime();
+
 private:
   static constexpr double ObstacleProbability = 0.2;
   static constexpr int MinObstacleInterval = 3;
   static constexpr int MaxObstacleInterval = 8;
+  static constexpr int MinObstacleLifetime = 2;
+  static constexpr int MaxObstacleLifetime = 5;
 
+  int m_currentTick = 0;
   int m_ticksSinceLastObstacle = 0;
   int m_nextObstacleInterval = MinObstacleInterval;
+  std::unordered_map<ScAddr, int, ScAddrHashFunc> m_obstacleExpirationTicks;
 
   std::mt19937 m_randomGenerator{std::random_device{}()};
 };
