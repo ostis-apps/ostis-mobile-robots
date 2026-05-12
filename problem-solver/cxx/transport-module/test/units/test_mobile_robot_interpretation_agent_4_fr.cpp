d063141363e44ca9d8e7c94ad1e4542016e104c5
TOP1KWER <vask_by41@mail.ru>
2026-05-12 13:30:22 +0300

    [solver][test] Fix test 4 fr

--- problem-solver/cxx/transport-module/test/units/test_mobile_robot_interpretation_agent_4_fr.cpp
+++ problem-solver/cxx/transport-module/test/units/test_mobile_robot_interpretation_agent_4_fr.cpp
@@ -1,17 +1,16 @@
 #include "test_utils.hpp"
-#include "mobile"
 
-TEST_F(TransportModuleTest, CallMobileRobotInterpretationAgent3fr)
+TEST_F(TransportModuleTest, CallMobileRobotInterpretationAgent4fr)
 {
   try
   {
     ScAgentContext context;
     ScsLoader loader;
-    loader.loadScsFile(context, EXAMPLE_MODULE_TEST_FILES_DIR_PATH + "example_3_fr.scs");
+    loader.loadScsFile(context, EXAMPLE_MODULE_TEST_FILES_DIR_PATH + "example_4_fr.scs");
 
     SubscribeAgents(context);
 
-    loader.loadScsFile(context, EXAMPLE_MODULE_TEST_FILES_DIR_PATH + "example_3_robots_initial_states_fr.scs");
+    loader.loadScsFile(context, EXAMPLE_MODULE_TEST_FILES_DIR_PATH + "example_4_robots_initial_states_fr.scs");
 
     sleep(15);
     UnsubscribeAgents(context);
