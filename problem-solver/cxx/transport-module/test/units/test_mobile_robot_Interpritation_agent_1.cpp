96be1a083a1848ba61a457457d69e9e5e3a2fffc
TOP1KWER <vask_by41@mail.ru>
2026-05-12 22:37:39 +0300

    tests not working

--- problem-solver/cxx/transport-module/test/units/test_mobile_robot_Interpritation_agent_1.cpp
+++ problem-solver/cxx/transport-module/test/units/test_mobile_robot_Interpritation_agent_1.cpp
@@ -1,16 +1,16 @@
 #include "test_utils.hpp"
 
-TEST_F(TransportModuleTest, CallMobileRobotCoordinationAgent11)
+TEST_F(TransportModuleTest, CallMobileRobotInterpritationAgent1)
 {
   try
   {
     ScAgentContext context;
     ScsLoader loader;
-    loader.loadScsFile(context, EXAMPLE_MODULE_TEST_FILES_DIR_PATH + "example_1.scs");
+    loader.loadScsFile(context, EXAMPLE_MODULE_TEST_FILES_DIR_PATH + "Test_robot_1_route.scs");
 
     SubscribeAgents(context);
 
-    loader.loadScsFile(context, EXAMPLE_MODULE_TEST_FILES_DIR_PATH + "example_1_robots_initial_states.scs");
+    loader.loadScsFile(context, EXAMPLE_MODULE_TEST_FILES_DIR_PATH + "Test_robot_1_state.scs");
 
     sleep(5);
     // переписать на ожидание событий остановки всех роботов
